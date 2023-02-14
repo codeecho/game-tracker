f1bb7898f933c97347ec30205a9538cc5ad61024
Adam Knight <adam.knight@datalanguage.com>
2023-02-14 16:18:59 +0000

    Fixed null pointer

--- pages/api/sync.js
+++ pages/api/sync.js
@@ -2,7 +2,16 @@ import datagraphs from '../../clients/datagraphs';
 
 const handler = async (req, res) => {
     const state = req.body;
-    await datagraphs.concepts.upsertBulk('urn:game-backlog:games', state.games.map(x => ({ ...x, id: `urn:game-backlog:Game:${x.id}`})));
+    const games = state.games.map(x => {
+        const res = {};
+        Object.entries(x).forEach(([key, value]) => {
+            if(value !== null) res[key] = value;
+        });
+        res.id = `urn:game-backlog:Game:${x.id}`;
+        return res;
+    });
+    console.log(games);
+    await datagraphs.concepts.upsertBulk('urn:game-backlog:games', games);
     res.status(200).send();
 };
 
